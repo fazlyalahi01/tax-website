83c0fe7caaa442cbab217ed33c2fb9c9c3489ae9
riyazulhaque64 <riyazulhaque64@gmail.com>
2024-08-24 12:44:47 +0600

    update resend email

--- src/services/actions/index.ts
+++ src/services/actions/index.ts
@@ -13,7 +13,7 @@ export const sendEmail = async (data: TContactFormData) => {
     const resend = new Resend(process.env.RESEND_API_KEY);
     const sent = await resend.emails.send({
       from: "onboarding@resend.dev",
-      to: "riyazulhaque64@gmail.com",
+      to: "zillurincometaxnyc@gmail.com",
       subject: data.subject,
       text: `Name: ${data.name}\nEmail: ${data.email}\nPhone: ${data.phone}\nMessage: ${data.message}`,
     });
